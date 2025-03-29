b22c5e79f1bd5d80dc1ec17274dd8ba62691056e
Marwan77770 <200039952@must.edu.eg>
2025-03-29 05:13:30 +0200

    Add files via upload

---
+++ lexer.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <cctype>
+#include <cstdio>
+using namespace std;
+
+/* Global declarations */
+int charClass;
+char lexeme[100];
+char nextChar;
+int lexLen;
+int nextToken;
+FILE *in_fp;
+
+/* Character classes */
+#define LETTER 1
+#define DIGIT 2
+#define UNKNOWN 99
+
+/* Token codes */
+#define LEFT_PAREN 00
+#define IDENT 11
+#define ADD_OP 22
+#define INT_LIT 33
+#define MULT_OP 40
+#define RIGHT_PAREN 44
+#define DIV_OP 55
+#define EOF_TOKEN 77
+
+/* Function declarations */
+void addChar();
+void getChar();
+void getNonBlank();
+int lex();
+int lookup(char ch);
+
+/* Main driver */
+int main() {
+    if ((in_fp = fopen("front.in", "r")) == NULL)
+        cout << "ERROR - cannot open front.in" << endl;
+    else {
+        getChar();
+        do {
+            lex();
+        } while (nextToken != EOF_TOKEN);
+    }
+    return 0;
+}
+
+/* lookup - a function to lookup operators and parentheses and return the token */
+int lookup(char ch) {
+    switch (ch) {
+        case '(':
+            addChar();
+            nextToken = LEFT_PAREN;
+            break;
+        case ')':
+            addChar();
+            nextToken = RIGHT_PAREN;
+            break;
+        case '+':
+            addChar();
+            nextToken = ADD_OP;
+            break;
+        case '-':
+            addChar();
+            nextToken = ADD_OP;
+            break;
+        case '*':
+            addChar();
+            nextToken = MULT_OP;
+            break;
+        case '/':
+            addChar();
+            nextToken = DIV_OP;
+            break;
+        default:
+            addChar();
+            nextToken = EOF_TOKEN;
+            break;
+    }
+    return nextToken;
+}
+
+/* addChar - a function to add nextChar to lexeme */
+void addChar() {
+    if (lexLen <= 98) {
+        lexeme[lexLen++] = nextChar;
+        lexeme[lexLen] = 0;
+    } else
+        cout << "Error - lexeme is too long" << endl;
+}
+
+/* getChar - a function to get the next character of input and determine its character class */
+void getChar() {
+    if ((nextChar = getc(in_fp)) != EOF) {
+        if (isalpha(nextChar))
+            charClass = LETTER;
+        else if (isdigit(nextChar))
+            charClass = DIGIT;
+        else
+            charClass = UNKNOWN;
+    } else
+        charClass = EOF_TOKEN;
+}
+
+/* getNonBlank - a function to call getChar until it returns a non-whitespace character */
+void getNonBlank() {
+    while (isspace(nextChar))
+        getChar();
+}
+
+/* lex - a simple lexical analyzer for arithmetic expressions */
+int lex() {
+    lexLen = 0;
+    getNonBlank();
+    switch (charClass) {
+        case LETTER:
+            addChar();
+            getChar();
+            while (charClass == LETTER || charClass == DIGIT) {
+                addChar();
+                getChar();
+            }
+            nextToken = IDENT;
+            break;
+        case DIGIT:
+            addChar();
+            getChar();
+            while (charClass == DIGIT) {
+                addChar();
+                getChar();
+            }
+            nextToken = INT_LIT;
+            break;
+        case UNKNOWN:
+            lookup(nextChar);
+            getChar();
+            break;
+        case EOF_TOKEN:
+            nextToken = EOF_TOKEN;
+            lexeme[0] = 'E';
+            lexeme[1] = 'O';
+            lexeme[2] = 'F';
+            lexeme[3] = 0;
+            break;
+    }
+    cout << "Next token is: " << nextToken << ", Next lexeme is " << lexeme << endl;
+    return nextToken;
+}
